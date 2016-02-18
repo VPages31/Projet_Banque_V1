3685b4c7ab4c1c255b0f73afd0de51a088407251
VPages31 <VPages31@gmail.com>
2016-02-18 10:39:37 +0100

    maj CptEpargne

--- src/cptEpargne.cpp
+++ src/cptEpargne.cpp
@@ -1,1 +1,78 @@
 #include "cptEpargne.h"
+
+    //Constructeur
+    CptEpargne::CptEpargne(string numCompte, int plaf, double inte):Compte(num)
+    {   interets=i;
+        plafond=plaf;
+        #ifdef DEBUG
+            cout << "Constructeur par parametres cptEpargne" << endl;
+        #endif // DEBUG
+    }
+    
+    //Affectation
+	cptEpargne &cptEpargne::operator= (const cptEpargne &c)
+	{ Compte::numCompte=c.Compte::numCompte;
+		Compte::solde=c.Compte::solde;
+		for (int i=0; i<10; i++)
+		{
+			Compte::historique[i] = c.Compte::historique[i];
+		}
+		Compte::D=c.Compte::D;
+		interets=c.interets;
+		plafond=c.plaf;
+		#ifdef DEBUG
+			cout << "operator= CptEpargne " << (int)(this)<<endl;
+		#endif
+		return (*this);
+	}
+
+    void CptEpargne::Consulter()
+    { cout << endl <<  << endl;
+    }
+
+    void CptEpargne::Ajouter (double montant);
+    {Ajouter(montant);
+    }
+
+    void CptEpargne::Retirer (double montant);
+    { Retirer(montant);
+    }
+
+
+	void CptEpargne::Menu()
+	{
+		//CLEAR;
+		Ligne();
+		cout<< " / / / / / / COMPTE EPARGNE \\ \\ \\ \\ \\ \\ "<<endl;
+		Ligne();
+		AfficherCompte();
+		Ligne();
+		cout<<endl<< " 1: Ajouter de l'argent"<<endl;
+		cout<<endl<< " 0: Retour"<<endl;
+		char choix;
+		cin>>choix;
+		Poubelle(); //si jamais l'utilisateur entre plus d'1 caractere, on vide le cache
+		#ifdef DEBUG
+			cout << "votre choix: "<<choix<<endl;
+		#endif
+		switch(choix)
+		{
+		case 1:
+			#ifdef DEBUG
+				cout << "case 1 " <<endl;
+			#endif
+			break;
+		case 0:
+			#ifdef DEBUG
+				cout << "case 0 " <<endl;
+			#endif
+			break;
+		}
+	}
+
+    CptEpargne::~CptEpargne()
+    {
+        #ifdef DEBUG
+            cout << "Destructeur compte epargne " << (int)(this)<<endl;
+        #endif
+    }
--- src/cptEpargne.h
+++ src/cptEpargne.h
@@ -4,9 +4,19 @@ using namespace std;
 #ifndef CPTEPARGNE_H
 #define CPTEPARGNE_H
 
-class CptEpargne
+class CptEpargne : public Compte
 {
-	
+private:
+    int plafond;
+    double interets;
 
+public:
+	CptEpargne(string numCompte="123456",int plafond =15999, double interets=10);
+	CptEpargne &operator=(const CptEpargne &c);
+    void Consulter(); // Permet de consulter le solde et l'historique du compte
+    void Ajouter (double montant); // Ajoute de l'argent au solde
+    void Retirer (double montant); // Retire de l'argent au solde
+    void Menu();
+    ~CptEpargne();
 };
 #endif	// CPTEPARGNE_H
