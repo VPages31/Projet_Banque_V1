c48942f6190e86e6d240f91bd7a900f956be37ce
karima4582 <kari_info2001@yahoo.fr>
2016-02-19 14:03:52 +0100

    insertion des focntions de le menu

--- src/cptCourant.cpp
+++ src/cptCourant.cpp
@@ -6,7 +6,6 @@
 CptCourant::CptCourant(string num, int A): Compte(num)
 {
     agios=A;
-    #ifdef DEBUG
 		cout << "Constructeur cptCourant parametres " << (int)(this)<<endl;
 	#endif
 }
@@ -57,7 +56,7 @@ void CptCourant::Retirer ( double montant ) // Retire de l'argent au solde de co
         Compte::solde=Compte::solde-montant;
 
 }
- double CptCourant::RetirerAgio()
+ void  CptCourant::RetirerAgio()
     {
 
         if(Compte::solde < 0)
@@ -75,43 +74,58 @@ double CptCourant::GetSolde ()
 }
 void CptCourant::Menu()
 {
-    //CLEAR;
-    Ligne();
-    cout<< " **************\   COMPTE COURANT   /************** "<<endl;
-    Ligne();
-    AfficherCompte();
-    Ligne();
-    cout<<endl<< " 1: Ajouter de l'argent"<<endl;
-    cout<<endl<< " 2: Retirer de l'argent"<<endl;
-    cout<<endl<< " 3: Retirer agios"<<endl;
-    cout<<endl<< " 0: Retour"<<endl;
+
     char choix;
-    cin>>choix;
-    Poubelle(); //si jamais l'utilisateur entre plus d'1 caractere, on vide le cache
-    #ifdef DEBUG
-        cout << "votre choix: "<<choix<<endl;
-    #endif
-    switch(choix)
+    do
     {
-    case 1:
+        CLEAR;
+        Ligne();
+        cout<< " **************/ COMPTE BLOQUE \************** "<<endl;
+        Ligne();
+        AfficherCompte();
+        Ligne();
+        cout<<endl<< " 1: Ajouter de l'argent"<<endl;
+        cout<<endl<< " 2: Retirer de l'argent"<<endl;
+        cout<<endl<< " 3: Retirer Agios "<<endl;
+        cout<<endl<< " 0: Retour"<<endl;
+        cin>>choix;
+        Poubelle(); //si jamais l'utilisateur entre plus d'1 caractere, on vide le cache
         #ifdef DEBUG
-            cout << "case 1 " <<endl;
+            cout << "votre choix: "<<choix<<endl;
         #endif
-        break;
-    case 2:
-        #ifdef DEBUG
-            cout << "case 2 " <<endl;
-        #endif
-        break;
-    case 3:
-        #ifdef DEBUG
-            cout << "case 3 " <<endl;
-        #endif
-        break;
-    case 0:
-        #ifdef DEBUG
-            cout << "case 0 " <<endl;
-        #endif
-        break;
-    }
-}
+        double montant=0.0;
+        switch(choix)
+        {
+        case '1':
+            #ifdef DEBUG
+                cout << "case 1 " <<endl;
+            #endif
+            cout<< " Montant a ajouter: ";
+            cin>> montant;
+            this->Ajouter(montant);
+            break;
+        case '2':
+            #ifdef DEBUG
+                cout << "case 2 " <<endl;
+            #endif
+            cout<< " Montant a retirer: ";
+            cin>> montant;
+            this->Retirer(montant);
+            break;
+        case '3':
+            #ifdef DEBUG
+                cout << "case 3 " <<endl;
+            #endif
+            this->RetirerAgio();
+            break;
+        case '0':
+            #ifdef DEBUG
+                cout << "case 0 " <<endl;
+            #endif
+            break;
+        } //fin switch
+        cout<< "Appuyez sur Entree pour continuer"<<endl;
+        Poubelle(); // vide le cache et met en pause avant d'effacer l'ecran
+    } //fin do
+    while (choix!='0');
+} // fin Menu()
