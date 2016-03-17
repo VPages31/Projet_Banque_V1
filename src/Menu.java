f789f06c96caa46c24168c35cd791a42fec2b485
benoitn <benoit@nappey.fr>
2016-03-17 16:46:08 +0100

    presque
    tout fonctionne sauf l'ajout de véhicule s'il en reste qui attendent au
    feu

--- src/Menu.java
+++ src/Menu.java
@@ -14,10 +14,12 @@ public class Menu {
 	//Donnees Membres
 	protected int nbLigne = 55;
 	protected int temps=100;
-	protected int nbVehicules; //nombre total de vehicules dans le programme (passés et en attente)
+	protected int nbVehiculesTotal; //nombre total de vehicules dans le programme (passés et en attente)
+	protected int enAttente; //nombre de vehicules au feu
 	protected Hashtable<Integer, Vehicule> bouchon;
 	protected Feu feu;
 	protected Scanner sc;
+	protected String bouchon_s;
 	
 	public Menu() {
 		try { this.bouchon = new Hashtable<Integer, Vehicule>(); }
@@ -26,30 +28,33 @@ public class Menu {
 			catch (Exception e) { }
 		try { this.sc = new Scanner(System.in); }
 			catch (Exception e) { }
-		this.nbVehicules = 0;
+		this.nbVehiculesTotal = 0;
+		this.enAttente = 0;
 	}
 	
 	public void call() {
 		if (this.bouchon.size() < 1) {
-			initialize();
+			ajouterVehicules();
 		}
 		else { System.out.println("un bouchon existe"); }
-		ligne();
-		System.out.println(" ---  _ _   _ _    _ ____    _ _     _    _     _  --- ");
-		System.out.println(" --- | | \\_/ | |  | |____|  | | \\   | |  | |   | | --- ");
-		System.out.println(" --- | |\\   /| |  | |___    | |\\ \\  | |  | |   | | --- ");
-		System.out.println(" --- | | \\_/ | |  | |___|   | | \\ \\ | |  | |   | | --- ");
-		System.out.println(" --- | |     | |  | |____   | |  \\ \\| |  | |___| | --- ");
-		System.out.println(" --- |_|     |_|  |_|____|  |_|   \\_|_|  \\_______/ --- \n");
-		ligne();
-		System.out.println("\n 1 : Simulation automatique (pendant le temps de votre choix)");
-		System.out.println(" 2 : Simulation automatique (faire passer tous les vehicules)");
-		System.out.println(" 3 : Gestion manuelle du feu");
-		System.out.println(" 4 : Ajouter des vehicules au feu");
-		System.out.println("\n 0 : Quitter");
-		ligne();
+		
 		int choix;
 		do {
+			ligne();
+			System.out.println(" ---  _ _   _ _    _ ____    _ _     _    _     _  --- ");
+			System.out.println(" --- | | \\_/ | |  | |____|  | | \\   | |  | |   | | --- ");
+			System.out.println(" --- | |\\   /| |  | |___    | |\\ \\  | |  | |   | | --- ");
+			System.out.println(" --- | | \\_/ | |  | |___|   | | \\ \\ | |  | |   | | --- ");
+			System.out.println(" --- | |     | |  | |____   | |  \\ \\| |  | |___| | --- ");
+			System.out.println(" --- |_|     |_|  |_|____|  |_|   \\_|_|  \\_______/ --- \n");
+			ligne();
+			this.AfficherBouchon();
+			System.out.println("\n 1 : Simulation automatique (pendant le temps de votre choix)");
+			System.out.println(" 2 : Simulation automatique (faire passer tous les vehicules)");
+			System.out.println(" 3 : Gestion manuelle du feu");
+			System.out.println(" 4 : Ajouter des vehicules au feu");
+			System.out.println("\n 0 : Quitter");
+			ligne();
 			choix = sc.nextInt();
 			switch (choix) {
 			case 1:
@@ -62,13 +67,13 @@ public class Menu {
 				feuManuel();
 				break;
 			case 4:
-				initialize();
+				ajouterVehicules();
 				break;
 			case 0:
 				System.out.println("Merci d'avoir utilise notre programme");
 				break;
 			default:
-				System.out.println("Choix non reconnu");
+				System.out.println("Choix du menu non reconnu");
 				break;
 			}
 		}
@@ -82,29 +87,41 @@ public class Menu {
 		System.out.print("\n");
 	}
 	
-	public void initialize() {
+	public void ajouterVehicules() {
+		System.gc();
 		System.out.println("Merci de remplir la file de voiture\nsous la forme: 11221111212122...\navec: 1 pour moto, 2 pour voiture");
-		String bouchon_s = sc.nextLine();
+		if (sc.hasNext() && this.nbVehiculesTotal > 0) {
+			String poubelle=sc.nextLine();
+		}
+		bouchon_s = sc.nextLine();
 		int taille= bouchon_s.length();
+		System.out.println("nb de vehicules a ajouter : " +taille);
 		Moto moto = new Moto();
 		Voiture voiture = new Voiture();
-		for (int i=nbVehicules; i<taille ; i++) {
+		int id=this.bouchon.size();
+		for (int i=id; i<(id+taille) ; i++) {
 			switch (bouchon_s.charAt(i)) {
 			case '1':
-				try { this.bouchon.put(i, moto); }
-					catch (Exception e) { }
+				System.out.println(" ajout d'une moto " +i);
+				try { this.bouchon.putIfAbsent(i, moto); }
+					catch (Exception e) { System.out.println("impossible de creer le vehicule");}
+				this.nbVehiculesTotal++;
+				this.enAttente++;
 				break;
 			case '2':
-				try { this.bouchon.put(i, voiture); }
-				catch (Exception e) { }
+				System.out.println(" ajout d'une voiture " +i);
+				try { this.bouchon.putIfAbsent(i, voiture); }
+				catch (Exception e) { System.out.println("impossible de creer le vehicule"); }
+				this.nbVehiculesTotal++;
+				this.enAttente++;
 				break;
 			default:
 				System.out.println("Erreur: le caractere "+ (i+1) +" n'est pas reconnu");
 				break;
 			}
 		}
-		this.AfficherBouchon(0);
-		this.nbVehicules=this.nbVehicules + taille;
+		System.out.println("bouchon: "+this.bouchon.size());
+		this.AfficherBouchon();
 	}
 	
 	/**
@@ -136,9 +153,10 @@ public class Menu {
 					enCours.action(false);
 					System.out.println(tps_total);
 					bouchon.remove(i);
+					this.enAttente--;
 					i++;
 				}
-				AfficherBouchon(i); // affichage des vehicules restant
+				AfficherBouchon(); // affichage des vehicules restant
 				temps=temps-feu.dureeAvance;
 			}
 			else {
@@ -166,9 +184,10 @@ public class Menu {
 				enCours.action(false);
 				System.out.println(tps_total);
 				bouchon.remove(i);
+				this.enAttente--;
 				i++;
 			}
-			AfficherBouchon(i); // affichage des vehicules restant
+			AfficherBouchon(); // affichage des vehicules restant
 		}
 		while (i<taille);
 	}
@@ -187,13 +206,11 @@ public class Menu {
 		int choix = 9;
 		int num=0;
 		Vehicule enCours = bouchon.get(0);
-		while((bouchon.size() > 0) && choix != 0)
+		while((this.enAttente > 0) && choix != 0)
 		{
-			for(int i = 0; i < bouchon.size(); ++i) 
-			{
-				enCours = bouchon.get(num);
+			enCours = bouchon.get(num);
 				System.out.println("Vehicule suivant : " + enCours.nom );
-				System.out.println("1 - Laisser passer le vehicule suivant \n 2 - Changer la couleur du feu \n 3 - Retour");
+				System.out.println("1 - Laisser passer le vehicule suivant \n 2 - Changer la couleur du feu \n 0 - Retour");
 				choix = sc.nextInt();
 				switch(choix) 
 				{
@@ -203,6 +220,7 @@ public class Menu {
 						enCours.action(false); // = feu rouge faux--> donc passageOK
 						bouchon.remove(enCours); // le vehicule passe, il est supprimme de la liste
 						num++;
+						this.enAttente--;
 					}
 					else
 					{
@@ -218,15 +236,17 @@ public class Menu {
 					// retour
 					break;
 				}
-			}
+			
 		}
 	}
 
-	public void AfficherBouchon(int n) {
-		int taille = this.bouchon.size();
+	public void AfficherBouchon() {
+		//int taille = this.enAttente;
+		int taille = bouchon.size();
+		int n = 0;//this.nbVehiculesTotal - this.enAttente;
 		if(bouchon.size() > 0)	{
 			ligne();
-			System.out.println("<Vehicules dans la file: ");
+			System.out.println("<Vehicules dans la file: "+taille);
 			for (int i=n; i<taille; i++) {
 				Vehicule enCours = this.bouchon.get(i);
 				System.out.print(" "+ enCours.nom);
